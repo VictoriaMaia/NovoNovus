e9f968cb8ae1bf5fae01c19012959dbf34e447bd
italo yeltsin <br.yeltsin@gmail.com>
2020-02-14 14:51:40 +0000

    Sidemenu

--- src/main/java/com/totalcross/view/SideMenuContainerView.java
+++ src/main/java/com/totalcross/view/SideMenuContainerView.java
@@ -1,4 +1,28 @@
 package com.totalcross.view;
 
-public class SideMenuContainerView {
+
+import com.totalcross.util.Colors;
+import com.totalcross.view.components.SideButtons;
+import totalcross.ui.Container;
+import totalcross.ui.gfx.Color;
+import totalcross.util.UnitsConverter;
+
+public abstract class SideMenuContainerView extends Container {
+
+    private Container content = new Container();
+    private SideButtons sideButtons = new SideButtons();
+
+    public SideMenuContainerView() {
+
+    }
+
+    @Override
+    public void initUI() {
+        content.setBackColor(Colors.COLOR_BLACK);
+        add(sideButtons, LEFT, TOP, UnitsConverter.toPixels(DP + 104), PARENTSIZE);
+        add(content, AFTER, TOP, FILL, PARENTSIZE);
+        onView(content);
+    }
+
+    public abstract void onView(Container content);
 }
